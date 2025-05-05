e9e183fa025d7074f8605b507d6273ac20eaa382
Duy Ngo <ngohoangkhanhduy.work@gmail.com>
2025-05-05 21:53:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,19 @@
 import streamlit as st
 import pandas as pd
 import datetime
-
-# --- Import all utility blocks ---
-from utils import (
-    block_a_data,
-    block_b_factor,
-    block_c_covariance,
-    block_d_forecast,
-    block_e_feasibility,
-    block_e1_visualization,
-    block_e2_visualization,
-    block_f_backtest,
-    block_g_optimization,
-    block_h_complete_portfolio,
-    block_i_performance_analysis,
-    block_j_stress_testing,
-)
+from utils import config
+from utils import block_a_data
+from utils import block_b_factor
+from utils import block_c_covariance
+from utils import block_d_forecast
+from utils import block_e_feasibility
+from utils import block_e1_visualization
+from utils import block_e2_visualization
+from utils import block_f_backtest
+from utils import block_g_optimization
+from utils import block_h_complete_portfolio
+from utils import block_i_performance_analysis
+from utils import block_j_stress_testing
 
 # --- App Config ---
 st.set_page_config(page_title="Institutional Portfolio Optimization", layout="wide")
@@ -38,7 +35,7 @@ start_date = st.sidebar.date_input("Start Date", datetime.date(2020, 1, 1))
 end_date = st.sidebar.date_input("End Date", datetime.date.today())
 rf_annual = st.sidebar.number_input("Annual Risk-Free Rate (%)", value=9.0)
 total_capital = st.sidebar.number_input("Total Capital (VND)", value=750_000_000)
-risk_aversion = st.sidebar.slider("Risk Aversion (A)", 1, 10, value=5)
+risk_aversion = st.sidebar.slider("Risk Aversion (A)", 10, 40, value=15)
 
 # Derived config
 rf_monthly = rf_annual / 12 / 100
