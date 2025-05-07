eaf8488c34c61e0be05b5e23ffd163fc6ee06604
Duy Ngo <ngohoangkhanhduy.work@gmail.com>
2025-05-07 15:36:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ if run_analysis:
             block_h1_visualization.display_portfolio_info(portfolio_info, alloc_df)
             st.success("H1 – Portfolio Summary Displayed")
 
-            block_h2_visualization.run(capital_alloc, portfolio_info['capital_rf'], portfolio_info['capital_risky'], tickers_portfolio)
+            block_h2_visualization.run(capital_alloc, config.total_capital, tickers_portfolio)
             st.success("H2 – Allocation Visualized")
 
             if isinstance(returns_benchmark.index, pd.PeriodIndex):
